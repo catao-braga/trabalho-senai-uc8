914cfb43b804e1aa8aef6e91d50096f62e0c4114
catao-braga <guilherme19catao@gmail.com>
2021-10-27 14:49:11 -0300

    V .01 - Atv

---
+++ projeto_pecas.js
@@ -0,0 +1,14 @@
+
+
+// CONTAGEM DE PEÇAS 
+
+let pecas = ['Parafuso', 'Placa', 'Solda', 'Maçaneta', 'Roupa'];
+let i = 0; 
+
+for (items of pecas)
+{
+    console.log(`${items}\n`);
+    i++;
+}
+
+console.log(`Número de peças ${i}`)
